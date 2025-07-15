081e6f51e25e24bfafbe38c728032063df8cccdf
baileynance <baieyj.nance@gmail.com>
2025-07-15 12:23:49 -0500

    mcw mvie

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f30d11d0e93e94421c86aa2f3578a29cd8f94f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3ac093c6e64741c66c1036c645189f31708ead")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
